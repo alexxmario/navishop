df9abcce829b67d76d323045617d1fe38948df3f
Alex Ionescu <alexionescu870@gmail.com>
2026-01-08 16:39:07 +0200

    Fix debugP5 script to use MongoDB Atlas connection from .env

--- backend/scripts/debugP5.js
+++ backend/scripts/debugP5.js
@@ -1,9 +1,10 @@
+require('dotenv').config();
 const mongoose = require('mongoose');
 const Product = require('../models/Product');
 
 async function debugP5() {
   try {
-    await mongoose.connect('mongodb://127.0.0.1:27017/piloton');
+    await mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/piloton');
     console.log('Connected to MongoDB');
 
     // Check P5 product
